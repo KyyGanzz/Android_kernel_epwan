275f77474dcb6d5dca1a6a9fd4522b1f3b7254d6
NekoTuru10 <kirmansyah9696@gmail.com>
2023-10-20 17:53:08 +0000

    Revert "init: Add CONFIG_INITRAMFS_IGNORE_SKIP_FLAG"
    This reverts commit 75b0634082f8ca268bbd01c6785a5a6d38b8609b.

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -9,40 +9,6 @@ static char new_command_line[COMMAND_LINE_SIZE];
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
 	seq_printf(m, "%s\n", saved_command_line);
-#include <asm/setup.h>
-#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
-#include <asm/setup.h>
-#endif
-
-#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
-#define INITRAMFS_STR_FIND "skip_initramf"
-#define INITRAMFS_STR_REPLACE "want_initramf"
-#define INITRAMFS_STR_LEN (sizeof(INITRAMFS_STR_FIND) - 1)
-
-static char proc_command_line[COMMAND_LINE_SIZE];
-
-static void proc_command_line_init(void) {
-	char *offset_addr;
-
-	strcpy(proc_command_line, saved_command_line);
-
-	offset_addr = strstr(proc_command_line, INITRAMFS_STR_FIND);
-	if (!offset_addr)
-		return;
-
-	memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
-}
-#endif
-
-static char new_command_line[COMMAND_LINE_SIZE];
-
-static int cmdline_proc_show(struct seq_file *m, void *v)
-{
-#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
-	seq_printf(m, "%s\n", proc_command_line);
-#else
-	seq_printf(m, "%s\n", new_command_line);
-#endif
 	return 0;
 }
 
@@ -84,17 +50,6 @@ static void patch_safetynet_flags(char *cmd)
 
 static int __init proc_cmdline_init(void)
 {
-#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
-	proc_command_line_init();
-#endif
-	strcpy(new_command_line, saved_command_line);
-
-	/*
-	 * Patch various flags from command line seen by userspace in order to
-	 * pass SafetyNet checks.
-	 */
-	patch_safetynet_flags(new_command_line);
-
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
