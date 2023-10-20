9c5d1ac5a58d5237db1463ee5d439e80a6dd1c2a
NekoTuru10 <kirmansyah9696@gmail.com>
2023-10-20 17:51:00 +0000

    Revert "arm64/configs: Enable CONFIG_INITRAMFS_IGNORE_SKIP_FLAG"
    This reverts commit d77bfdb8d1f6d99e74a9439aa5bb57c0f58af5d4.

--- compile.sh
+++ compile.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Edit by Neko
-# Configure by Suzurui
+#Edit by Neko
 
 # init
 function init() {
@@ -8,34 +7,33 @@ function init() {
     echo "= START COMPILING KERNEL  ="
     echo "==========================="
 }
-# Main
+# Main 
 function compile() {
 
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
 
-    export KBUILD_BUILD_USER="NekoTuru"
+    export KBUILD_BUILD_USER="Neko_Turu"
     export KBUILD_BUILD_HOST="Weeaboo"
-
+    
     export ARCH=arm64
     export SUBARCH=arm64
-
-    export PATH="/home/neko/weebx-clang/bin:${PATH}"
-    export LD_LIBRARY_PATH="/home/neko/weebx-clang/lib:$LD_LIBRARY_PATH"
+    export PATH="/workspace/ehhe/proton-clang/bin:${PATH}"
+    export LD_LIBRARY_PATH="/workspace/ehhe/proton-clang/lib:$LD_LIBRARY_PATH"
 
     make -j$(nproc --all) O=out ARCH=arm64 beryllium_defconfig
     make -j$(nproc --all) O=out \
                         ARCH=arm64 \
-			LD=ld.lld \
-			CC=clang \
-			CLANG_TRIPLE=aarch64-linux-gnu- \
-			CROSS_COMPILE=aarch64-linux-gnu- \
-			CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+                        CC=clang \
+                        LD=ld.lld \
+                        CROSS_COMPILE=aarch64-linux-gnu- \
+                        CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+    
 }
 #end
 function end(){
     echo "==========================="
-    echo "=  COMPILE KERNEL COMPLET ="
-    echo "=  Santai Dulu Ngga Sih   ="
+    echo "=  COMPILE KERNEL COMPLETE    ="
+    echo "=    Santai Dulu Ngga Sih    ="
     echo "==========================="
 }
 
