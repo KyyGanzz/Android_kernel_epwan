6d28461a48d2573774fef1ca9c1250bd281ef4bb
NekoTuru10 <kirmansyah9696@gmail.com>
2023-11-02 09:20:22 +0000

    Revert "touchscreen: nt36xxx: Restore OEM data reading functionality"
    This reverts commit 2a8673bcf415662177ce9f1dd869cb874bd3f1db.

--- drivers/input/touchscreen/nt36xxx/nt36xxx.h
+++ drivers/input/touchscreen/nt36xxx/nt36xxx.h
@@ -229,6 +229,7 @@ extern void nvt_stop_crc_reboot(void);
 
 extern int32_t Init_BootLoader(void);
 extern int32_t Resume_PD(void);
+extern int32_t nvt_get_lockdown_info(char *lockdata);
 
 /*******************************************************
 Description:
