4a5e4763f171af6c91bd067fe6ba060a6727683a
NekoTuru10 <kirmansyah9696@gmail.com>
2023-12-26 16:02:37 +0000

    Reapply "kernel: Use task struct instead of pid to check for zygote"
    This reverts commit 0e1ea214752de95c145fafd31026068e3c7169b5.

--- kernel/fork.c
+++ kernel/fork.c
@@ -2113,9 +2113,10 @@ long _do_fork(unsigned long clone_flags,
 	long nr;
 
 	/* Boost CPU to the max for 50 ms when userspace launches an app */
-	if (is_zygote_pid(current->pid)) {
-		cpu_input_boost_kick_max(50);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
+	if (task_is_zygote(current)) {
+		cpu_input_boost_kick_max(1000);
+		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 1000);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1000);
 	}
 
 	/*
